dda6e6cabd3e8fffcc6704ccf781bea72f511e27
Nicosvge <nicolas.sauvagevllaroy@gmail.com>
2018-01-18 16:37:42 +0100

    js header + jquery

---
+++ js/header.js
@@ -0,0 +1,46 @@
+$(document).ready(function()
+{
+ 
+    
+    $( "#accueil" ).hover(
+  function() {
+    $( "#submenu-index" ).css('visibility', 'visible');
+  }, function() {
+    $( "#submenu-index" ).css('visibility', 'hidden');
+  }
+        );
+        
+     $( "#master" ).hover(
+  function() {
+    $( "#submenu-master" ).css('visibility', 'visible');
+  }, function() {
+    $( "#submenu-master" ).css('visibility', 'hidden');
+  }
+         );
+    
+      $( "#admission" ).hover(
+  function() {
+    $( "#submenu-admission" ).css('visibility', 'visible');
+  }, function() {
+    $( "#submenu-admission" ).css('visibility', 'hidden');
+  }
+        );
+        
+    $( "#realisation" ).hover(
+  function() {
+    $( "#submenu-realisation" ).css('visibility', 'visible');
+  }, function() {
+    $( "#submenu-realisation" ).css('visibility', 'hidden');
+  }
+);
+            $( "#mondepro" ).hover(
+  function() {
+    $( "#submenu-mondePro" ).css('visibility', 'visible');
+  }, function() {
+    $( "#submenu-mondePro" ).css('visibility', 'hidden');
+  }
+     
+        
+);
+});
+                  
