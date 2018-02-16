9e98db6fafbe38b6d5ac281bafde4ebc0d919f9c
Charaf <charaf297@gmail.com>
2018-02-16 16:27:59 +0100

    statéstiques

---
+++ js/stats.js
@@ -0,0 +1,32 @@
+
+    $( document ).ready(function() {
+	$("#students").circliful({
+                animationStep: 5,
+                foregroundBorderWidth: 7,
+                backgroundBorderWidth: 0,
+
+                percent: 75
+           });
+   	});
+
+   	$( document ).ready(function() {
+	$("#passing").circliful({
+                animationStep: 5,
+                foregroundBorderWidth: 7,
+                backgroundBorderWidth: 0,
+                foregroundColor: "#FFB32A",
+                percent: 75
+           });
+   	});
+
+
+   	$( document ).ready(function() {
+	$("#working").circliful({
+                animationStep: 5,
+                foregroundBorderWidth: 7,
+                backgroundBorderWidth: 0,
+                foregroundColor: "#FC4F25",
+                percent: 75
+           });
+   	});
+
