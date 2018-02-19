aa234920a05584c3666a919f5d132f7d9c13fd9b
Nicosvge <nicolas.sauvagevllaroy@gmail.com>
2018-02-19 23:09:14 +0100

    pull realisation

--- js/header.js
+++ js/header.js
@@ -8,48 +8,39 @@ $(document).ready(function()
       $( "#submenu-index" ).css('visibility', 'visible');
   }, function() {
       
-
-
+      $( "#submenu-index" ).css('visibility', 'hidden');
   }
 );
-    
-          $( "#submenu-index" ).hover(
-      function() {
-        $( "#submenu-index" ).css('visibility', 'visible');
-      }, function() {
-          $( "#submenu-index" ).css('visibility', 'hidden');
-      }
-    );
     
     
      $( "#master" ).hover(
   function() {
     $( "#submenu-master" ).css('visibility', 'visible');
   }, function() {
-   // $( "#submenu-master" ).css('visibility', 'hidden');
+    $( "#submenu-master" ).css('visibility', 'hidden');
   }
-         );
+ );
     
       $( "#admission" ).hover(
   function() {
     $( "#submenu-admission" ).css('visibility', 'visible');
   }, function() {
-  //  $( "#submenu-admission" ).css('visibility', 'hidden');
+    $( "#submenu-admission" ).css('visibility', 'hidden');
   }
-        );
+);
         
     $( "#realisation" ).hover(
   function() {
     $( "#submenu-realisation" ).css('visibility', 'visible');
   }, function() {
-   // $( "#submenu-realisation" ).css('visibility', 'hidden');
+    $( "#submenu-realisation" ).css('visibility', 'hidden');
   }
 );
             $( "#mondepro" ).hover(
   function() {
     $( "#submenu-mondePro" ).css('visibility', 'visible');
   }, function() {
-   // $( "#submenu-mondePro" ).css('visibility', 'hidden');
+    $( "#submenu-mondePro" ).css('visibility', 'hidden');
   }
      
         
