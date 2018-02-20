46bc180e99d982df476a2a5939a9a434e57c6505
Charaf <charaf297@gmail.com>
2018-02-20 21:28:03 +0100

    Home page

--- js/stats.js
+++ js/stats.js
@@ -1,32 +1,50 @@
 
     $( document ).ready(function() {
 	$("#students").circliful({
-                animationStep: 5,
+                animationStep: 4.5,
                 foregroundBorderWidth: 7,
                 backgroundBorderWidth: 0,
-
-                percent: 75
+                percent: 80,
+                textAdditionalCss : "font-family : abel",
+                percentageY : 110,
+                animateInView : true,
+                text : "Etudiants par année",
+                textBelow : true,
+                textStyle : "font-family : abel; font-size : 12px;",
+                foregroundColor:"#45B8E5",
            });
    	});
 
    	$( document ).ready(function() {
 	$("#passing").circliful({
-                animationStep: 5,
+                animationStep: 4.5,
                 foregroundBorderWidth: 7,
                 backgroundBorderWidth: 0,
-                foregroundColor: "#FFB32A",
-                percent: 75
+                foregroundColor: "#C71D82",
+                percent: 95,
+                percentageY : 110,
+                textAdditionalCss : "font-family : abel",
+                animateInView : true,
+                text : "Taux de réussite chaque année",
+                textBelow : true,
+                textStyle : "font-family : abel; font-size : 12px;",
            });
    	});
 
 
    	$( document ).ready(function() {
 	$("#working").circliful({
-                animationStep: 5,
+                animationStep: 4.5,
                 foregroundBorderWidth: 7,
                 backgroundBorderWidth: 0,
-                foregroundColor: "#FC4F25",
-                percent: 75
+                foregroundColor: "#512DA8",
+                percent: 85,
+                percentageY : 110,
+                textAdditionalCss : "font-family : abel",
+                animateInView : true,
+                text : "Taux d'embauche chaque année",
+                textBelow : true,
+                textStyle : "font-family : abel; font-size : 12px;",
            });
    	});
 
