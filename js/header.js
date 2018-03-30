5dad9900e4bf9193a402a75f216c42415db9d4b4
Unknown <a.tojo.antonio@gmail.com>
2018-03-30 02:54:25 +0200

    menu responsive functional

--- js/header.js
+++ js/header.js
@@ -1,54 +1,28 @@
 $(document).ready(function() {
 
-  var menuButton = $('#menuButton');
-  menuButton.on('click', function (e) {
+  var menuButton = $("#menuButton");
+  var subMenu = $(".js-open-submenu");
+
+  menuButton.on("click", function(e) {
     e.preventDefault();
-    menuButton.toggleClass('is-active');
+    $(this).toggleClass("is-active");
     $("#js-menu-container").slideToggle("slow");
   });
-    
- $( "#accueil" ).hover(
-  function() {
-      
-      $( "#submenu-index" ).css('visibility', 'visible');
-  }, function() {
-      
-      $( "#submenu-index" ).css('visibility', 'hidden');
-  }
-);
-    
-    
-     $( "#master" ).hover(
-  function() {
-    $( "#submenu-master" ).css('display', 'block');
-  }, function() {
-    $( "#submenu-master" ).css('display', 'none');
-  }
- );
-    
-      $( "#admission" ).hover(
-  function() {
-    $( "#submenu-admission" ).css('display', 'block');
-  }, function() {
-    $( "#submenu-admission" ).css('display', 'none');
-  }
-);
-        
-    $( "#realisation" ).hover(
-  function() {
-    $( "#submenu-realisation" ).css('display', 'block');
-  }, function() {
-    $( "#submenu-realisation" ).css('display', 'none');
-  }
-);
-            $( "#mondepro" ).hover(
-  function() {
-    $( "#submenu-mondePro" ).css('display', 'block');
-  }, function() {
-    $( "#submenu-mondePro" ).css('display', 'none');
-  }
-     
-        
-);
+
+  subMenu.on("click", function(e) {
+    //skip if already open
+    if($(this).parent().hasClass("is-active")) {
+      return;
+    }
+    if(subMenu.parent().hasClass("is-active")) {
+      //we have menu opened, so close it before open another
+      subMenu.parent().find(".submenu").css("display", "none");
+      subMenu.parent().removeClass("is-active");
+    }
+    $(this).parent().toggleClass("is-active");
+    $(this).parent().find(".submenu").slideToggle("slow");
+  });    
+
+
 });
                   
